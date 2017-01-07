4486c70d288aaf0355e69527fd96bbc9d5712c4c
Hoang Vo <hoang.vominh96@gmail.com>
2017-01-08 00:05:37 +0200

    adding actions to the project

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
 
 class BookList extends Component {
           renderList() {
                     return this.props.books.map((book) => {
                               return (
-                                        <li key={book.title} className="list-group-item">
+                                        <li
+                                                  key={book.title}
+                                                  className="list-group-item"
+                                                  onClick={() => this.props.selectBook(book)}
+                                        >
                                                   {book.title}
                                         </li>
                               )
@@ -30,4 +36,14 @@ function mapStateToProps(state) {
           };
 }
 
-export default connect(mapStateToProps)(BookList);
+// Anything returns from this function will end up as props
+// on the BookList containers
+function mapDispatchToProps(dispatch){
+          // Whenever selectBook is called, the result should be passed
+          // to all of our reducers
+          return bindActionCreators({ selectBook: selectBook }, dispatch);
+}
+
+// Promote BookList form a component to a container - it needs to now
+// about this new dispatch method, selectBook. Make it available as a prop.
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function () {
           return [
-                    {title: 'Javascript: The good Parts'},
-                    {title: 'Harry Porter'},
-                    {title: 'The dark tower'},
-                    {title: 'Eloquent Ruby'}
+                    {title: 'Javascript: The good Parts', pages: 85},
+                    {title: 'Harry Porter', pages: 85},
+                    {title: 'The dark tower', pages: 85},
+                    {title: 'Eloquent Ruby', pages: 85},
           ];
 }
