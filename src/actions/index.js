5e3693bef2114c64f345ce4d24c3a8645ba7f90b
Hoang Vo <hoang.vominh96@gmail.com>
2017-01-08 13:57:16 +0200

    adding request with axios

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = "c574b870ed21bfb282568ec5841a5431";
+const ROOT_URL= `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+          const url = `${ROOT_URL}&q=${city},fi`;
+          const request = axios.get(url);
+
+          return {
+                    type: FETCH_WEATHER,
+                    payload: request
+          };
+}
