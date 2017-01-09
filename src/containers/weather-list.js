0c80c216cd2b92d26f88ad05e693bd3cd6469452
Hoang Vo <hoang.vominh96@gmail.com>
2017-01-09 13:12:51 +0200

    adding chartlines and finish up the site

--- src/containers/weather-list.js
+++ src/containers/weather-list.js
@@ -1,7 +1,23 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import Chart from '../components/chart';
 
 class WeatherList extends Component{
+          renderWeather(cityData){
+                    const name = cityData.city.name;
+                    const temps = cityData.list.map(weather => weather.main.temp);
+                    const pressures = cityData.list.map(weather => weather.main.pressure);
+                    const humidities = cityData.list.map(weather => weather.main.humidity);
+                    return (
+                              <tr key={name}>
+                                        <td>{name}</td>
+                                        <td><Chart data={temps} color={"orange"}></Chart></td>
+                                        <td><Chart data={pressures} color={"green"}></Chart></td>
+                                        <td><Chart data={humidities} color={"black"}></Chart></td>
+                              </tr>
+                    )
+          }
+
           render(){
                     return (
                               <table className="table table-hover">
@@ -13,7 +29,9 @@ class WeatherList extends Component{
                                                             <th>Himidity</th>
                                                   </tr>
                                         </thead>
-                                        <tbody></tbody>
+                                        <tbody>
+                                                  {this.props.weather.map(this.renderWeather)}
+                                        </tbody>
                               </table>
                     )
           }
